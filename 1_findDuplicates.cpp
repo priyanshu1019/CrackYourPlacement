60e4b694160c8ef029fe6041147a589305fdb191
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-07-14 19:02:24 +0530

    first question

---
+++ 1_findDuplicates.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+
+        int n = nums.size();
+        for (int i = 0; i < n;)
+        {
+            int correct = nums[i] - 1;
+            if (nums[correct] != nums[i])
+            {
+                swap(nums[correct], nums[i]);
+            }
+            else
+            {
+                i++;
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] != (i + 1))
+            {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+};
