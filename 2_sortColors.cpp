976d24e45822e8b08a31f00fe8e45fd7f8031953
Prianshu goutam <96710150+priyanshu1019@users.noreply.github.com>
2024-07-14 19:03:31 +0530

    2nd question

---
+++ 2_sortColors.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int low = 0, mid = 0;
+        int n = nums.size();
+        int high = n - 1;
+
+        while (mid <= high)
+        {
+            if (nums[mid] == 2)
+            {
+                swap(nums[mid], nums[high--]);
+            }
+            else if (nums[mid] == 0)
+            {
+                swap(nums[low++], nums[mid++]);
+            }
+            else
+            {
+                mid++;
+            }
+        }
+    }
+};
